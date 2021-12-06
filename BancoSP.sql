b7a7b4be1348b92a5ae561f96c099dccc506c9d6
calebmg <borismoran2000@hotmail.com>
2021-12-06 11:41:40 -0600

    Validaciones de transacción

--- BancoSP.sql
+++ BancoSP.sql
@@ -70,3 +70,140 @@ exec SPCreditAccount '123003', '123.00'
 exec SPInsertUser 'Ana', 'Ana Karina', 'Ortiz Fuentes', '2000-02-23', 'Ciudad', '38238992', 'ana123'
 
 delete from Cuentahabiente where Usuario = 'AndreaH'
+---------------
+
+
+--Validar que lo tenga en contactos y en el receptor este HABILITADO
+CREATE OR ALTER FUNCTION F_ValidarTercero(@emisor NVARCHAR(50), @receptor NVARCHAR(50), @cuentaR INT)
+RETURNS NVARCHAR(50)
+AS
+BEGIN
+	DECLARE @tercero NVARCHAR(50)
+	DECLARE @estado BIT --El estado del tercero
+
+	SET @tercero = (SELECT Tercero.[No. Cuenta] FROM Tercero 
+	INNER JOIN Cuenta 
+		ON Tercero.Cuentahabiente = Cuenta.Cuentahabiente
+	WHERE Tercero.Usuario = @receptor AND Tercero.Cuentahabiente = @emisor AND Tercero.[No. Cuenta] = @cuentaR)
+
+	SET @estado = (SELECT Cuenta.Estado FROM Tercero 
+	INNER JOIN Cuenta 
+		ON Tercero.Usuario = Cuenta.Cuentahabiente
+	WHERE Cuenta.Cuentahabiente = @receptor AND Cuenta.[No. Cuenta] = @cuentaR )
+	
+	IF @estado = 0
+	BEGIN
+		SET @tercero = 'Disable'
+	END
+
+	RETURN ISNULL(@tercero,'NotExist')
+END
+GO
+
+SELECT dbo.F_ValidarTercero('boris', 'lucas', 123457) AS CuentaATransferir
+go
+-----------------
+
+--Validación de monto, si tiene la cantidad ingresada retoran 1, y si no posee la cantidad retorna 0
+CREATE OR ALTER FUNCTION F_ValidarMonto(@emisor NVARCHAR(50), @cuenta INT, @monto MONEY)
+RETURNS BIT
+AS
+BEGIN
+	DECLARE @valido BIT
+	IF @monto > (SELECT Saldo FROM Cuenta WHERE [No. Cuenta] = @cuenta)
+	BEGIN
+		SET @valido = 0
+	END
+	ELSE
+	BEGIN
+		SET @valido = 1
+	END
+	
+
+	RETURN @valido
+END
+GO
+
+SELECT dbo.F_ValidarMonto('boris', 123456, 900)
+go
+-----------------
+
+--Update, realiza el debito en cuenta emisor y el cretido en receptor
+CREATE OR ALTER PROCEDURE SPUpdateSaldos
+	@emisor		INT,
+	@receptor	INT,
+	@monto		MONEY
+AS
+BEGIN
+	DECLARE @anteriorE	MONEY, 
+			@actualE	MONEY, 
+			@anteriorR	MONEY, 
+			@actualR	MONEY
+
+	SET @anteriorE = (SELECT Saldo FROM Cuenta WHERE [No. Cuenta] = @emisor)
+	SET @anteriorR = (SELECT Saldo FROM Cuenta WHERE [No. Cuenta] = @receptor)
+	SET @actualE = (@anteriorE - @monto)
+	SET @actualR = (@anteriorR + @monto)
+
+	UPDATE Cuenta
+	SET Saldo = @actualE
+	WHERE [No. Cuenta] = @emisor
+
+	UPDATE Cuenta
+	SET Saldo = @actualR
+	WHERE [No. Cuenta] = @receptor
+
+END
+GO
+
+EXEC SPUpdateSaldos 123457, 123456, 300.00
+GO
+
+-----------------
+
+--Trigger de insert de transacción, insertar dos registros
+CREATE OR ALTER TRIGGER TRTransaccionRealizada
+ON Transaccion FOR INSERT
+AS
+	DECLARE @emisor 	NVARCHAR(50);
+	DECLARE @cuentaE	INT;
+	DECLARE @receptor	NVARCHAR(50);
+	DECLARE @cuentaR	INT;
+	DECLARE @monto		NVARCHAR(50);
+	DECLARE @fechahora	DATETIME;
+
+	DECLARE @ValTercero	NVARCHAR(50);
+	DECLARE @ValMonto	BIT;
+
+	SELECT @cuentaE = CuentaEmisor FROM inserted
+	SELECT @emisor = Cuentahabiente FROM Cuenta WHERE [No. Cuenta] = @cuentaE
+	SELECT @cuentaR = CuentaReceptor FROM inserted
+	SELECT @receptor = Cuentahabiente FROM Cuenta WHERE [No. Cuenta] = @cuentaR
+	
+	SELECT @monto = Monto FROM inserted
+	SELECT @fechahora = [Fecha y hora] FROM inserted
+
+	SET @ValTercero = (SELECT dbo.F_ValidarTercero(@emisor, @receptor, @cuentaR))
+
+	IF (@ValTercero <> @cuentaR)
+	BEGIN
+		PRINT(@ValTercero)
+		RETURN
+	END
+
+	SET @ValMonto = (SELECT dbo.F_ValidarMonto(@emisor, @cuentaE, @monto))
+
+	IF (@ValMonto = 0)
+	BEGIN
+		PRINT('Saldo insuficiente')
+		RETURN
+	END
+
+	IF ((@ValTercero = @cuentaR) AND (@ValMonto = 1))
+	BEGIN
+		EXEC SPUpdateSaldos @cuentaE, @cuentaR, @monto
+	END
+
+	INSERT INTO Transaccion VALUES (@cuentaE, @cuentaR, @monto, 'C', @fechahora)
+	--PRINT(@emisor + @cuentaE + @receptor + @cuentaR + @monto + 'C' + @fechahora)
+GO
